7b0b684b1e27a46db5c5eb833283ef59be4e6e8a
Anish Raj Pandey <anishrajpandey@Mac-15625.lan>
2025-04-14 02:30:03 -0500

    changed hero text

--- app/page.tsx
+++ app/page.tsx
@@ -2,8 +2,8 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-  <div>
+  <section>
     <Image src={'/signature_dark.png'} alt="Anish Raj Pandey" width={200} height={200} />
-    Welcome to my website</div>
+    Welcome to my website</section>
   );
 }
