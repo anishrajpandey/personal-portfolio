aa097a2a2d7c41dda441240179cd3c585b8fbf4e
Anish Raj Pandey <anishrajpandey@Mac-15625.lan>
2025-04-14 02:27:31 -0500

    changed hero text

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,6 @@ export default function Home() {
   return (
   <div>
     <Image src={'/signature_dark.png'} alt="Anish Raj Pandey" width={200} height={200} />
-    Hello world</div>
+    Welcome to my website</div>
   );
 }
