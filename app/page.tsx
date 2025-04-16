863056523f589ffc839f7a22858659a485a9912d
Anish Raj Pandey <anishpandey021@gmail.com>
2025-04-15 23:57:01 -0500

    model optimization

--- app/page.tsx
+++ app/page.tsx
@@ -2,27 +2,35 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-  <section>
+    <section className="relative">
+      <div className="flex justify-center  w-screen h-screen overflow-hidden scky">
+        <div className=" w-[calc(100vw - 8rem)] h-10/12 relative pointer-events-all">
+          <div className="fixed bg-white z-10 w-screen top-0 h-auto">
+            <Image
+              src={"/signature_dark.png"}
+              alt="Anish Raj Pandey"
+              width={200}
+              height={200}
+            />
+            Website is in progress ...
+          </div>
+          <div className="text-white absolute bg-white w-screen bottom-0 h-auto z-10">
+            <div className="w-screen min-h-16 text-2xl text-center m-0 p-0">
+             
+            </div>
+          </div>
    
-    
-    <div className="flex justify-center  w-screen h-screen overflow-hidden bg-[rgb(26,31,123)]"> 
-      <div className=" w-[calc(100vw - 8rem)] h-10/12 relative pointer-events-all">
-      <div className="absolute bg-white z-10 w-screen top-0 h-auto">
-      <Image src={'/signature_dark.png'} alt="Anish Raj Pandey" width={200} height={200} />
-      Website is in progress  ...
+            
+          <iframe
+            className=" w-screen h-full"
+            src="https://sketchfab.com/models/890bbeb2ad5a4bd6b80df2089416aae7/embed?autostart=1&preload=1&transparent=0"
+          >
+            {" "}
+          </iframe>
+  
+        </div>
       </div>
 
-      <div className="absolute bg-[rgb(26,31,123)] z-10 w-screen bottom-0 h-16">
-
-        <div className="w-screen text-2xl text-white p-3 text-center">Hi, I am Anish </div>
-      </div>
-        
-
-      <iframe className="opacity-50 w-screen h-full"  src="https://sketchfab.com/models/890bbeb2ad5a4bd6b80df2089416aae7/embed?autostart=1&preload=1&transparent=1"> </iframe> 
-q  
-      </div>
-    
-    </div>
     </section>
   );
 }
