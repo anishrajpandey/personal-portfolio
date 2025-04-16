f1f11653ceecbe3152114107541fc3af6fa66d6e
Anish Raj Pandey <anishpandey021@gmail.com>
2025-04-15 22:47:18 -0500

    included a 3d model for hero section

--- app/page.tsx
+++ app/page.tsx
@@ -3,11 +3,24 @@ import Image from "next/image";
 export default function Home() {
   return (
   <section>
-    <Image src={'/signature_dark.png'} alt="Anish Raj Pandey" width={200} height={200} />
-    Website is in progress  ...
+   
     
-    <div className="sketchfab-embed-wrapper"> <iframe title="Doggy ( 11 animations, 2 skins )" frameBorder="0" allowFullScreen allow="autoplay; fullscreen; xr-spatial-tracking" execution-while-out-of-viewport= "true" execution-while-not-rendered web-share src="https://sketchfab.com/models/890bbeb2ad5a4bd6b80df2089416aae7/embed?autostart=1&preload=1&transparent=1"> </iframe> 
-    {/* <p style="font-size: 13px; font-weight: normal; margin: 5px; color: #4A4A4A;"> <a href="https://sketchfab.com/3d-models/doggy-11-animations-2-skins-890bbeb2ad5a4bd6b80df2089416aae7?utm_medium=embed&utm_campaign=share-popup&utm_content=890bbeb2ad5a4bd6b80df2089416aae7" target="_blank" rel="nofollow" style="font-weight: bold; color: #1CAAD9;"> Doggy ( 11 animations, 2 skins ) </a> by <a href="https://sketchfab.com/Lil_Pupinduy?utm_medium=embed&utm_campaign=share-popup&utm_content=890bbeb2ad5a4bd6b80df2089416aae7" target="_blank" rel="nofollow" style="font-weight: bold; color: #1CAAD9;"> Lil_Pupinduy </a> on <a href="https://sketchfab.com?utm_medium=embed&utm_campaign=share-popup&utm_content=890bbeb2ad5a4bd6b80df2089416aae7" target="_blank" rel="nofollow" style="font-weight: bold; color: #1CAAD9;">Sketchfab</a></p> */}
+    <div className="flex justify-center  w-screen h-screen overflow-hidden bg-[rgb(26,31,123)]"> 
+      <div className=" w-[calc(100vw - 8rem)] h-10/12 relative pointer-events-all">
+      <div className="absolute bg-white z-10 w-screen top-0 h-auto">
+      <Image src={'/signature_dark.png'} alt="Anish Raj Pandey" width={200} height={200} />
+      Website is in progress  ...
+      </div>
+
+      <div className="absolute bg-[rgb(26,31,123)] z-10 w-screen bottom-0 h-16">
+
+        <div className="w-screen text-2xl text-white p-3 text-center">Hi, I am Anish </div>
+      </div>
+        
+
+      <iframe className="opacity-50 w-screen h-full"  src="https://sketchfab.com/models/890bbeb2ad5a4bd6b80df2089416aae7/embed?autostart=1&preload=1&transparent=1"> </iframe> 
+q  
+      </div>
     
     </div>
     </section>
