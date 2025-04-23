8c3e7dfef67854b93d38d7079b4c51727f58bf65
Anish Raj Pandey <anishpandey021@gmail.com>
2025-04-23 13:28:15 -0500

    added animations delay for icons in homepage

--- app/page.tsx
+++ app/page.tsx
@@ -57,17 +57,17 @@ export default function Home() {
               {" "}
             </iframe>
             <div className=" parent pointer-events-none absolute  w-full h-auto  top-1/5 bottom-1/5  z-30 ">
-              <div className="div1 relative left-[250px] scale-200 rotate-45 flex justify-center items-center fill-mode-forwards transition-delay-300">
+              <div className="div1 opacity-0 relative left-[20rem] scale-150 flex justify-center items-center fill-mode-forwards transition-delay-300">
                 <Image
                   src={"/Brackets1.png"}
                   alt="Anish Raj Pandey"
-                  className="  hover:animate-bounce delay-75 transition-all duration-200 cursor-pointer"
+                  className="  "
                   width={100}
                   height={100}
                 />
               </div>
 
-              <div className="div2  relative left-1/3 scale-170  flex justify-center items-center forwa">
+              <div className="div2 opacity-0  relative left-1/3 scale-170  flex justify-center items-center forwa">
                 <Image
                   src={"/cloud-icon.png"}
                   alt="Anish Raj Pandey"
@@ -76,16 +76,16 @@ export default function Home() {
                   height={100}
                 />
               </div>
-              <div className="div  flex justify-center items-center forwa">
+              <div className="div6 opacity-0 flex justify-center items-center ">
                 <Image
                   src={"/vscode.webp"}
                   alt="Anish Raj Pandey"
-                  className="  hover:animate-bounce relative left-[450px] scale-75 delay-100 xl transition-all duration-200 cursor-pointer"
+                  className="  hover:animate-bounce relative bottom-[17rem] left-[30rem]  scale-75 transition-all cursor-pointer"
                   width={100}
                   height={100}
                 />
               </div>
-              <div className="div5  relative left-36 top-12 scale-110 flex justify-center items-center forwa">
+              <div className="div5 opacity-0  relative left-36 top-12 scale-110 flex justify-center items-center forwa">
                 <Image
                   src={"/coffee.png"}
                   alt="Anish Raj Pandey"
@@ -94,7 +94,7 @@ export default function Home() {
                   height={100}
                 />
               </div>
-              <div className="div4  relative right-[250px] flex justify-center items-center forwa">
+              <div className="div4 opacity-0  relative right-[250px] flex justify-center items-center forwa">
                 <Image
                   src={"/group 1.png"}
                   alt="Anish Raj Pandey"
@@ -103,7 +103,7 @@ export default function Home() {
                   height={150}
                 />
               </div>
-              <div className="div3 flex justify-center items-center forwa">
+              <div className="div3 opacity-0 flex justify-center items-center forwa">
                 <Image
                   src={"/headphone1.png"}
                   alt="Anish Raj Pandey"
